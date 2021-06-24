94927408413b7fc19da631594cc0f23e2c876a15
KeyC0de <nik.lazkey@gmail.com>
2021-06-24 15:37:58 +0300

    refactoring

--- UseTheDll/main.cpp
+++ UseTheDll/main.cpp
@@ -5,14 +5,14 @@
 #pragma region dllLinkage
 // include this:
 //#include "..\dll_example\dll_example.h"
-// or use dllimport:
+// or use dllimport (and I will):
 #ifdef  __cplusplus
 // prevents name mangling, thus allowing the dll to be used by both C and C++ programs
 #	define IMPORT extern "C" __declspec( dllimport )
 #else
 #	define IMPORT __declspec( dllexport )
 #endif
-// declare the dll functions you'll use with dllimport below:
+
 #ifdef UNICODE
 IMPORT BOOL CALLBACK CenterTextW( HDC, PRECT, PCWSTR );
 #	define CenterText CenterTextW
@@ -36,6 +36,7 @@ LRESULT CALLBACK WndProc( HWND hwnd,
 	switch ( message )
 	{
 	case WM_PAINT:
+	{
 		hdc = BeginPaint( hwnd,
 			&ps );
 		GetClientRect( hwnd,
@@ -43,14 +44,16 @@ LRESULT CALLBACK WndProc( HWND hwnd,
 		CenterTextW( hdc,
 			&rect,
 			TEXT( "This string was displayed using a dll's function." ) );
-		//TextOut(hdc, 100, 100, nonDllStr, sizeof(nonDllStr) * 12);
+		//TextOut( hdc, 100, 100, nonDllStr, sizeof(nonDllStr) * 12 );
 		EndPaint( hwnd,
 			&ps );
 		return 0;
-
+	}
 	case WM_DESTROY:
+	{
 		PostQuitMessage( 0 );
 		return 0;
+	}
 	}
 
 	return DefWindowProc( hwnd,
@@ -59,13 +62,14 @@ LRESULT CALLBACK WndProc( HWND hwnd,
 		lParam );
 }
 
-// This program centers a text string in its client area,
-// acquiring the function to do so from the dll
+
+// This program centers a text string in its client area, acquiring the function to do so from the dll (dll_example)
 int WINAPI WinMain( HINSTANCE hInst,
 	HINSTANCE hPrevInst,
 	PSTR szCmdLine,
 	int iCmdShow )
 {
+	UNREFERENCED_PARAMETER( hPrevInst );
 	static TCHAR szClassName[] = TEXT("UseTheDllExample");
 	HWND hwnd;
 	MSG msg;
@@ -76,8 +80,10 @@ int WINAPI WinMain( HINSTANCE hInst,
 	wndClass.cbClsExtra = 0;
 	wndClass.cbWndExtra = 0;
 	wndClass.hInstance = hInst;
-	wndClass.hIcon = LoadIconW( nullptr, IDI_APPLICATION );
-	wndClass.hCursor = LoadCursorW( nullptr, IDC_ARROW );
+	wndClass.hIcon = LoadIconW( nullptr,
+		IDI_APPLICATION );
+	wndClass.hCursor = LoadCursorW( nullptr,
+		IDC_ARROW );
 	wndClass.hbrBackground = (HBRUSH)GetStockObject( WHITE_BRUSH );
 	wndClass.lpszMenuName = nullptr;
 	wndClass.lpszClassName = szClassName;
@@ -85,14 +91,14 @@ int WINAPI WinMain( HINSTANCE hInst,
 	if ( !RegisterClassW( &wndClass ) )
 	{
 		MessageBox( nullptr,
-			TEXT("This program requires Windows NT!"),
+			TEXT( "This program requires Windows NT!" ),
 			szClassName,
 			MB_ICONERROR );
 		return 0;
 	}
 
 	hwnd = CreateWindowW( szClassName,
-		TEXT("DLL Demo Program"),
+		TEXT( "DLL Demo Program" ),
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
